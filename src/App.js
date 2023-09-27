91ed51ac954438f7bf092348837c00350a4682b0
jackson1129 <jacksoncoco29@gmail.com>
2023-09-28 01:13:12 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,52 @@
-
 import "./App.css"
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import useClipboard from "react-use-clipboard";
-import {useState} from "react";
+import { useState } from "react";
 
 
 const App = () => {
     const [textToCopy, setTextToCopy] = useState();
     const [isCopied, setCopied] = useClipboard(textToCopy, {
-        successDuration:1000
+        successDuration: 1000
     });
 
-    const language='en-IN'
+    const [lan,setLan]=useState('English input')
+    const [languages,setLanguages]= useState('hi-IN')
+    const handleSelect=()=>{
+        if (languages==='en-IN'){
+            setLanguages('hi-IN')
+            setLan('Hindi input')
+        } 
+        if (languages==='hi-IN'){
+            setLanguages('en-IN')
+            setLan('English input')
+        } 
+
+    }
 
 
-    const startListening = () => SpeechRecognition.startListening({ continuous: true, language: {language} });
+    const startListening = () => SpeechRecognition.startListening({ continuous: true, language: languages });
     const { transcript, browserSupportsSpeechRecognition } = useSpeechRecognition();
 
     if (!browserSupportsSpeechRecognition) {
         return null
     }
 
+    // const handleClick=()=>{
+    //     console.log(transcript);
+    //     alert(transcript)
+    // }
+
     return (
         <>
             <div className="container">
-                <h2>Speech to Text Converter</h2>
-                <br/>
-                <p>Give an input of english from the microphone and you will get the text.</p>
-
-                <div className="main-content" onClick={() =>  setTextToCopy(transcript)}>
+                <h2>Speech to Text converter</h2>
+                <br />
+                <p>Give an input of Hindi/english from the microphone and you will get the text.</p>
+                
+                <button onClick={handleSelect}>{lan}</button>
+                
+                <div className="main-content" onClick={() => setTextToCopy(transcript)}>
                     {transcript}
                 </div>
 
@@ -37,9 +55,17 @@ const App = () => {
                     <button onClick={setCopied}>
                         {isCopied ? 'Copied!' : 'Copy to clipboard'}
                     </button>
-                    <button onClick={startListening}>Start Listening</button>
-                    <button onClick={SpeechRecognition.stopListening}>Stop Listening</button>
+                    {/* <button onClick={startListening}>Start Listening</button>
+                    <button onClick={SpeechRecognition.stopListening}>Stop Listening</button> */}
 
+                    <button
+                        // onTouchStart={startListening}
+                        onMouseDown={startListening}
+                        onTouchEnd={SpeechRecognition.stopListening}
+                        onMouseUp={SpeechRecognition.stopListening}
+                    >Hold to talk</button>
+                    <button onClick={()=>window.location.reload()}>clear</button>
+                    {/* <button onClick={handleClick}>set translation</button> */}
                 </div>
 
             </div>
